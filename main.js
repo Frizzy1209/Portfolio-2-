44b2e9a53086876fd2ded1b8e29a646d112b6632
frizzy1209 <kloecker1997@live.dk>
2022-09-28 11:48:12 +0200

    Updated sheets added button

--- main.js
+++ main.js
@@ -9,3 +9,37 @@ sunGlassClicker.addEventListener(`click`, function(){
     document.querySelector(`#score`).innerHTML = speed;
 })
 
+// Buttons
+
+let img = document.querySelector(`.sunGlass`)
+
+let btn1 = document.querySelector(`#btn1`)
+let btn2 = document.querySelector(`#btn2`)
+let btn3 = document.querySelector(`#btn3`)
+let btn4 = document.querySelector(`#btn4`)
+let btn5 = document.querySelector(`#btn5`)
+let btn6 = document.querySelector(`#btn6`)
+
+btn1.addEventListener(`click`, function (){
+    img.src = `Level1.png`;
+})
+
+btn2.addEventListener(`click`, function (){
+    img.src = `Level2.png`;
+})
+
+btn3.addEventListener(`click`, function (){
+    img.src = `Level3.png`;
+})
+
+btn4.addEventListener(`click`, function (){
+    img.src = `Level4.png`;
+})
+
+btn5.addEventListener(`click`, function (){
+    img.src = `Level5.png`;
+})
+
+btn6.addEventListener(`click`, function (){
+    img.src = `Level6.png`;
+})
