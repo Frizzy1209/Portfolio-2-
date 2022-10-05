e123875468abc55667651aebb3fd2ac37d2372f9
frizzy1209 <kloecker1997@live.dk>
2022-10-05 20:44:36 +0200

    Cleaned up JavaScript

--- main.js
+++ main.js
@@ -8,19 +8,22 @@ const sunGlassClicker = document.querySelector(`#sunGlass`)
 
 
 
+
 // The Clicker -----
+// every click gives more speed
 
 let speed = 0;
 
-sunGlassClicker.addEventListener(`click`, function()
-{
-    if (sunGlassClicker)
+sunGlassClicker.addEventListener
+(`click`, function()
     {
-        speed++;
+        if (sunGlassClicker)
+        {
+            speed++;
+        }
+        totalSpeedScore.innerHTML = speed;
     }
-    totalSpeedScore.innerHTML = speed;
-})
-
+)
 
 
 
@@ -28,7 +31,6 @@ sunGlassClicker.addEventListener(`click`, function()
 
 // Upgrade Buttons -----
 // The buttons that lets you change teh sunglasses
-
 
 const img = document.querySelector(`#sunGlass`)
 
@@ -41,71 +43,78 @@ const btn5 = document.querySelector(`#btn5`)
 
 // First upgrade button - tha changes the sunglasses
 
-btn1.addEventListener(`click`, function ()
-{
-    if (speed >= (500))
+btn1.addEventListener
+(`click`, function ()
     {
-        speed = speed - (500);
-        img.src = `Level2.png`;
-        btn1.style.display = "none";
+        if (speed >= (500))
+        {
+            speed = speed - (500);
+            img.src = `Level2.png`;
+            btn1.style.display = "none";
+        }
+        totalSpeedScore.innerHTML = speed;
     }
-    totalSpeedScore.innerHTML = speed;
-})
+)
 
 // Second upgrade button - tha changes the sunglasses
 
-btn2.addEventListener(`click`, function ()
-{
-    if (speed >= (1000))
+btn2.addEventListener
+(`click`, function ()
     {
-        speed = speed - (1000);
-        img.src = `Level3.1.png`;
-        btn2.style.display = "none";
+        if (speed >= (1000))
+        {
+            speed = speed - (1000);
+            img.src = `Level3.1.png`;
+            btn2.style.display = "none";
+        }
+        totalSpeedScore.innerHTML = speed;
     }
-    totalSpeedScore.innerHTML = speed;
-})
+)
 
 // Third upgrade button - tha changes the sunglasses
 
-btn3.addEventListener(`click`, function ()
-{
-    if (speed >= (10000))
+btn3.addEventListener
+(`click`, function ()
     {
-        speed = speed - (10000);
-        img.src = `Level4.png`;
-        btn3.style.display = "none";
+        if (speed >= (10000))
+        {
+            speed = speed - (10000);
+            img.src = `Level4.png`;
+            btn3.style.display = "none";
+        }
+        totalSpeedScore.innerHTML = speed;
     }
-    totalSpeedScore.innerHTML = speed;
-})
+)
 
 // Forth upgrade button - tha changes the sunglasses
 
-btn4.addEventListener(`click`, function ()
-{
-    if (speed >= (30000))
+btn4.addEventListener
+(`click`, function ()
     {
-        speed = speed - (30000);
-        img.src = `Level5.png`;
-        btn4.style.display = "none";
+        if (speed >= (30000))
+        {
+            speed = speed - (30000);
+            img.src = `Level5.png`;
+            btn4.style.display = "none";
+        }
+        totalSpeedScore.innerHTML = speed;
     }
-    totalSpeedScore.innerHTML = speed;
-})
+)
 
 // Fifth upgrade button - tha changes the sunglasses
 
-btn5.addEventListener(`click`, function ()
-{
-    if (speed >= (75000))
+btn5.addEventListener
+(`click`, function ()
     {
-        speed = speed - (75000);
-        img.src = `Level6.png`;
-        btn5.style.display = "none";
+        if (speed >= (75000))
+        {
+            speed = speed - (75000);
+            img.src = `Level6.png`;
+            btn5.style.display = "none";
+        }
+        totalSpeedScore.innerHTML = speed;
     }
-    totalSpeedScore.innerHTML = speed;
-})
-
-
-
+)
 
 
 
@@ -115,32 +124,30 @@ btn5.addEventListener(`click`, function ()
 // Street credz button
 // Automatic "clicker"
 
-
 let autoClicker = 0;
 
 const autoCLickButton = document.querySelector(`#autoClickBtn`)
 const levelAutoClick = document.querySelector(`#autoClickLevel`)
 const costAutoCLick = document.querySelector(`#autoClickCost`)
 
-autoCLickButton.addEventListener(`click`, function ()
-{
-    if (speed >= ((autoClicker + 1) * 65))
+
+autoCLickButton.addEventListener
+(`click`, function ()
     {
-        speed = speed - (autoClicker + 1) * 65;
-        autoClicker = autoClicker + 1;
-        setInterval(function ()
+        if (speed >= ((autoClicker + 1) * 65))
         {
-            totalSpeedScore.innerHTML = speed++
-        }, 2000);
+            speed = speed - (autoClicker + 1) * 65;
+            autoClicker = autoClicker + 1;
+            setInterval(function ()
+            {
+                totalSpeedScore.innerHTML = speed++
+            }, 2000);
+        }
+        totalSpeedScore.innerHTML = speed;
+        levelAutoClick.innerHTML = autoClicker;
+        costAutoCLick.innerHTML = (autoClicker + 1) * 65;
     }
-    totalSpeedScore.innerHTML = speed;
-    levelAutoClick.innerHTML = autoClicker;
-    costAutoCLick.innerHTML = (autoClicker + 1) * 65;
-})
-
-
-
-
+)
 
 
 
@@ -150,7 +157,6 @@ autoCLickButton.addEventListener(`click`, function ()
 // Multi click button
 // Lets every click have more value
 
-
 let multiClicker = 0;
 
 const multiClickBtn = document.querySelector(`#multiClickBtn`)
@@ -158,18 +164,22 @@ const amountOfMultiCLick = document.querySelector(`#multiClick`)
 const costOfMultiClick = document.querySelector(`#multiClickCost`)
 
 
-multiClickBtn.addEventListener(`click`, function ()
-{
-    if (speed >= ((multiClicker + 1) * 35))
+multiClickBtn.addEventListener
+(`click`, function ()
     {
-        speed = speed - (multiClicker + 1) * 35;
-        multiClicker = multiClicker + 1;
-    }
-        sunGlassClicker.addEventListener(`click`, function ()
+        if (speed >= ((multiClicker + 1) * 35))
         {
-            speed++
-        })
-    totalSpeedScore.innerHTML = speed;
-    amountOfMultiCLick.innerHTML = multiClicker;
-    costOfMultiClick.innerHTML = (multiClicker + 1) * 35;
-})
+            speed = speed - (multiClicker + 1) * 35;
+            multiClicker = multiClicker + 1;
+        }
+            sunGlassClicker.addEventListener
+            (`click`, function ()
+                {
+                    speed++
+                }
+            )
+        totalSpeedScore.innerHTML = speed;
+        amountOfMultiCLick.innerHTML = multiClicker;
+        costOfMultiClick.innerHTML = (multiClicker + 1) * 35;
+    }
+)
